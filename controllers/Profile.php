47d0309cdfb751aec5000973714531699fbafe4e
Floryne <floryne.tourret.pro@gmail.com>
2019-04-16 19:02:43 +0200

    Front profil

--- controllers/Profile.php
+++ controllers/Profile.php
@@ -5,5 +5,13 @@ Class Profile extends Controller{
     public function index(){
         if(!isset($_SESSION['user']))
             header('Location: /');
+
+        $data = array();
+        //$this->loadModel('Profil_model');
+
+        $this->loadView('Base/header_view');
+        $this->loadView('Base/navbar_view');
+        $this->loadView('Profil/index_view', $data);
+        $this->loadView('Base/footer_view');
     }
 }
