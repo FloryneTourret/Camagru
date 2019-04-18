25c30e24f06c53bc294dd9b7865fdb7e1ac6b55c
Frederic Leonard <fleonard@z4r7p3.le-101.fr>
2019-04-18 16:24:20 +0200

    add image

--- models/Studio_model.php
+++ models/Studio_model.php
@@ -2,7 +2,11 @@
 
 Class Studio_model extends Model
 {
-    
+    public function addimg($path, $id)
+    {
+        $req = $this->db->prepare("INSERT INTO `pictures`(`picture_path`, `picture_user_id`) VALUES ('$path', $id)");
+        $req->execute();
+    }
 }
 
 ?>
