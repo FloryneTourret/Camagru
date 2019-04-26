2161a28cdbd7f1b23643c09c6cdde49b6b672ad9
Floryne <floryne.tourret.pro@gmail.com>
2019-04-26 15:58:12 +0200

    create db if not exist

--- config/database.php
+++ config/database.php
@@ -2,7 +2,8 @@
 
 class Database
 {
-    private $_server = 'mysql:host=172.18.0.2;dbname=camagru';
+    private $_serverdb = 'mysql:host=172.20.0.2;dbname=camagru';
+    private $_server = 'mysql:host=172.20.0.2';
     private $_user = 'root';
     private $_password = 'root';
     private $options = array(
@@ -14,7 +15,14 @@ class Database
     public function openConnection()
     {
         try {
-            $this->conn = new PDO($this->_server, $this->_user, $this->_password, $this->options);
+            $this->connexion = new PDO($this->_server, $this->_user, $this->_password, $this->options);
+            $req = $this->connexion->prepare("CREATE DATABASE IF NOT EXISTS `camagru`;");
+            $req->execute();
+        } catch (PDOException $e) {
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
+        try {
+            $this->conn = new PDO($this->_serverdb, $this->_user, $this->_password, $this->options);
             return $this->conn;
         } catch (PDOException $e) {
             echo "There is some problem in connection: " . $e->getMessage();
